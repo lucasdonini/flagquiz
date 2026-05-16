a6993c6a27219da86f889ad6583da79cd5bacc55
lucasdonini <lkd.lucas@gmail.com>
2026-05-16 16:06:28 -0300

    feat(learning): Module 2.3 -> Exercise 2.3

--- core/src/main/java/com/kluska/flagquiz/QuizScreen.kt
+++ core/src/main/java/com/kluska/flagquiz/QuizScreen.kt
@@ -4,36 +4,60 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.utils.Scaling
 import com.badlogic.gdx.utils.ScreenUtils
+import com.badlogic.gdx.utils.viewport.ScreenViewport
 
 class QuizScreen(private val game: FlagQuizGame) : Screen {
 
-    private lateinit var flag: Texture
+    private lateinit var stage: Stage
 
     override fun show() {
-        Logger.log("QuizScreen activated")
+        Logger.log("Activating QuizScreen...")
+        stage = Stage(ScreenViewport())
+        Gdx.input.inputProcessor = stage
 
-        flag = game.assets.get("flags/br.png", Texture::class.java)
-        Logger.log("br.png asset retrieved in QuizScreen")
+        val flag = game.assets.get("flags/br.png", Texture::class.java)
+
+        val root = Table().apply {
+            setFillParent(true)
+            center()
+
+            val labelStyle = Label.LabelStyle(game.fonts.body, Color.WHITE)
+            add(Label("Que país é esse?", labelStyle))
+            row()
+
+            add(Image(flag).apply { setScaling(Scaling.fit) })
+                .maxWidth(400f)
+                .maxHeight(260f)
+                .pad(24f)
+            row()
+        }
+
+        stage.addActor(root)
     }
 
     override fun render(delta: Float) {
         ScreenUtils.clear(Color.NAVY)
 
-        Logger.log("Rendering flag...")
-        val x = (Gdx.graphics.width - flag.width) / 2f
-        val y = (Gdx.graphics.height - flag.height) / 2f
-        game.batch.render {
-            draw(flag, x, y)
-            game.fonts.body.draw(this, "Que país é esse?", 50f, 100f)
-        }
+        stage.act(delta)
+        stage.draw()
     }
 
-    override fun resize(width: Int, height: Int) {}
+    override fun resize(width: Int, height: Int) {
+        stage.viewport.update(width, height, true)
+    }
+
     override fun pause() {}
     override fun resume() {}
     override fun hide() {}
 
-    override fun dispose() {}
+    override fun dispose() {
+        stage.dispose()
+    }
 
 }
